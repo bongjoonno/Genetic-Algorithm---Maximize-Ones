cbecb893e7a106ee12572b33cee2ac14ea9834ba
zachary <zachary.feryate@gmail.com>
2025-10-25 03:34:39 -0700

    tested and it works, with 100 generations/100 population size, gene size 10

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from repopulate import repopulate
 
 population = generate_population(POPULATION_SIZE)
 
-for _ in range(10):
+for _ in range(100):
     population_by_fitness_scores = sorted(population, key = lambda gene: gene.count('1'))
 
     top_50_percent = population_by_fitness_scores[POPULATION_SIZE//2:]
 
     children = repopulate(top_50_percent)
 
-    population = top_50_percent.extend(children)
+    population = top_50_percent + children
 
 print(population)
