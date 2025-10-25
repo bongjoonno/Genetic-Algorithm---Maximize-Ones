8004c9df3788ee048869d2103e0e3eb28366ddd3
zachary <zachary.feryate@gmail.com>
2025-10-25 02:57:02 -0700

    finished 1st draft of gen_population

--- generate_population.py
+++ generate_population.py
@@ -1,4 +1,15 @@
+from imports import choice
+from constants import GENE_LENGTH
+
 def generate_population(population_size):
+    population = []
 
     for _ in range(population_size):
+        gene = []
+
+        for _ in range(GENE_LENGTH):
+            gene.append(choice(['0', '1']))
         
+        population.append(''.join(gene))
+    
+    return population
