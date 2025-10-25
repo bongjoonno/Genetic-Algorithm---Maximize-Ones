36ea13e9cfe8108321527df919b3f29f7de9383b
zachary <zachary.feryate@gmail.com>
2025-10-25 03:04:25 -0700

    changed fitness function in favor of sorted() using key = gene.count('1')

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from constants import POPULATION_SIZE
 from generate_population import generate_population
 
 population = generate_population(POPULATION_SIZE)
-print(population)
+
+population_by_fitness_scores = sorted(population, key = lambda gene: gene.count('1'))
+
+top_50_percent = population_by_fitness_scores[POPULATION_SIZE//2:]
+
+print(len(population_by_fitness_scores), len(population), len(top_50_percent))
+
+# eliminate 50% of population
+
+# repopulate
