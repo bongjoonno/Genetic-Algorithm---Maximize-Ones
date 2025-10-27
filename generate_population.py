6b692eee58d610ca88e3856fbd0ca598dcf374e8
zachary <zachary.feryate@gmail.com>
2025-10-27 02:59:50 -0700

    changed gene_length from constants to something that is reset

--- generate_population.py
+++ generate_population.py
@@ -1,13 +1,13 @@
 from imports import choice
 from constants import GENE_LENGTH
 
-def generate_population(population_size):
+def generate_population(population_size, gene_length):
     population = []
 
     for _ in range(population_size):
         gene = []
 
-        for _ in range(GENE_LENGTH):
+        for _ in range(gene_length):
             gene.append(choice(['0', '1']))
         
         population.append(''.join(gene))
