aa65f1a718f8ad24db10b49ece450993432b6e7d
bongjoonno <zachary.feryate@gmail.com>
2025-10-28 02:55:32 -0700

    removed redundant double 1s count

--- genetic_evolution.py
+++ genetic_evolution.py
@@ -5,10 +5,9 @@ from mutate import mutate
 def genetic_evolution(population, perfect_chromosome, gene_split_method, gene_length, mutate_children: bool):
     epochs, epochs_w_no_progress = 0, 0
     population_set = set(population)
+    total_1s = sum(gene.count('1') for gene in population)
     
-    while perfect_chromosome not in population_set:
-        total_1s = sum(gene.count('1') for gene in population)
-        
+    while perfect_chromosome not in population_set:        
         population_by_fitness_scores = sorted(population, key = lambda gene: gene.count('1'))
 
         top_50_percent = population_by_fitness_scores[POPULATION_SIZE//2:]
@@ -21,11 +20,14 @@ def genetic_evolution(population, perfect_chromosome, gene_split_method, gene_le
         population = top_50_percent + children
         population_set = set(population)
         
-        if total_1s == sum(gene.count('1') for gene in population):
+        new_1s_count = sum(gene.count('1') for gene in population)
+        
+        if total_1s == new_1s_count:
             epochs_w_no_progress += 1
         if epochs_w_no_progress == 25:
             return 0
 
         epochs += 1
+        total_1s = new_1s_count
     
     return epochs
