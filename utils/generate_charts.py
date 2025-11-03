1024049377a9361aaf4f6bececca9718c692265f
zachary <zachary.feryate@gmail.com>
2025-11-03 03:00:34 -0800

    finished...

--- utils/generate_charts.py
+++ utils/generate_charts.py
@@ -4,7 +4,7 @@ from constants import MINIMUM_GENE_LENGTH, MAXIMUM_GENE_LENGTH, PROJECT_DIRECTOR
 FIGURE_PATH = PROJECT_DIRECTORY / 'convergence plots' / f'{MINIMUM_GENE_LENGTH}-{MAXIMUM_GENE_LENGTH} Gene Length'
 
 def generate_charts(genetic_operators_dict):
-        plt.figure(figsize=(12, 9))
+        plt.figure(figsize=(9, 9))
         
         for operator, information_dict in genetic_operators_dict.items():
                 plt.plot(information_dict['gene_lengths_to_convergences'].keys(), 
