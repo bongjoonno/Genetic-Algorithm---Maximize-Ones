02cff98d5ed4a3b77932ad2b10ed3b79dcddcf72
zachary <zachary.feryate@gmail.com>
2025-11-03 02:29:36 -0800

    changed step size to 20 and fixed generate_charts

--- utils/generate_charts.py
+++ utils/generate_charts.py
@@ -4,12 +4,13 @@ from constants import PROJECT_DIRECTORY, GENETIC_OPERATORS
 FIGURE_PATH = PROJECT_DIRECTORY / 'convergence plots' / 'plot.png'
 
 def generate_charts():
+        plt.figure(figsize=(12, 9))
+        
         for operator, information_dict in GENETIC_OPERATORS.items():
                 plt.plot(information_dict['gene_lengths_to_convergences'].keys(), 
                          information_dict['gene_lengths_to_convergences'].values(), 
                          label = operator)
 
-        plt.figure(figsize=(12, 9))
         plt.title('Genetic Operators | OneMax')
         plt.xlabel('Gene Length')
         plt.ylabel('Convergence Time (Generations)')
