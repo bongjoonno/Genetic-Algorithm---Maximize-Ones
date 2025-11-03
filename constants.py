4fa0439ab3f67ca02cb45243ab4b7f38c61564e4
zachary <zachary.feryate@gmail.com>
2025-11-03 02:26:25 -0800

    changed simulation_step_size to 10

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ POPULATION_SIZE = 400
 # Length of Gene must be > 2 as parental splitting is not possible without at least one gene per parent (e.g. at least 2)
 MINIMUM_GENE_LENGTH = 2
 MAXIMUM_GENE_LENGTH = 1000
-SIMULATION_STEP_SIZE = 5
+SIMULATION_STEP_SIZE = 10
 
 PROJECT_DIRECTORY = Path.cwd()
 
